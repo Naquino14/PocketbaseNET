72fb59976e50ace0c453af1e4c766ec9817afec1
Nathaniel Aquino <naquino14@outlook.com>
2022-12-25 23:00:46 -0500

    Added Nullable dictionary and SchemaField tests

---
+++ PocketbaseNETTests/models/utils/NullableDict.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketbaseNETTests.models.utils
+{
+    public class NullableDict<TKey, TValue> : Dictionary<TKey, TValue> where TKey : notnull
+    {
+        public new TValue? this[TKey k] 
+        {
+            get => ContainsKey(k) ? base[k] : default;
+            set => base[k] = value!;
+        }
+
+        public NullableDict() : base() { }
+
+        public NullableDict(IDictionary<TKey, TValue> dict) : base(dict) { }
+
+        public NullableDict(int capacity) : base(capacity) { }
+    }
+}
---
+++ PocketbaseNETTests/models/utils/SchemaFieldTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketbaseNET.models.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketbaseNET.models.utils.Tests
+{
+    [TestClass()]
+    public class SchemaFieldTests
+    {
+        [TestMethod()]
+        public void SchemaFieldTest()
+        {
+            var settings = new Dictionary<string, object>();
+
+            var id = "abcdefg123456";
+            settings["id"] = id;
+
+            var name = "test";
+            settings["name"] = name;
+
+            // var type = "text";
+            // settings["type"] = "text";
+            
+            bool system = true;
+            settings["system"] = system;
+            
+            bool required = true;
+            settings["required"] = required;
+
+            bool unique = true;
+            settings["unique"] = unique;
+
+            var testSchema = new SchemaField(settings);
+
+            Assert.AreEqual(id, testSchema.ID);
+            Assert.AreEqual(name, testSchema.Name);
+            Assert.AreEqual(system, testSchema.System);
+            Assert.AreEqual(required, testSchema.Required);
+            Assert.AreEqual(unique, testSchema.Unique);
+        }
+    }
+}
