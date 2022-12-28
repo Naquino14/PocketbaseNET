cd87cd094b60c1336830cb627e9e19e209785688
Nathaniel Aquino <naquino14@outlook.com>
2022-12-28 18:31:02 -0500

    BaseModel tests

---
+++ PocketbaseNET/models/utils/BaseModelTestClass.temp.cs
@@ -0,0 +1,14 @@
+﻿using PocketbaseNET.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketbaseNET.models.utils
+{
+    public class BaseModelTestClass : BaseModel
+    {
+        public BaseModelTestClass(NullableDictionary<string, object> data) : base(data) { }
+    }
+}
---
+++ PocketbaseNETTests/models/utils/BaseModelTestClassTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketbaseNET.models.utils;
+using PocketbaseNET.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketbaseNET.models.utils.Tests
+{
+    [TestClass()]
+    public class BaseModelTestClassTests
+    {
+        [TestMethod()]
+        public void BaseModelTestClassTest()
+        {
+            var data = new Dictionary<string, object>
+            {
+                { "id", "testid12345" },
+                { "created", "12/28/22" },
+                { "updated", "" },
+            };
+            BaseModel model = new BaseModelTestClass(NullableDictionary.FromDictToNullableDict(data));
+
+            Assert.AreEqual("testid12345", model.ID);
+            Assert.AreEqual("12/28/22", model.CreatedAt);
+            Assert.AreEqual("", model.UpdatedAt);
+        }
+
+        [TestMethod()]
+        public void BaseModelTestClassTest1()
+        {
+            var data = new Dictionary<string, object?>
+            {
+                { "id", "testid12345" },
+                { "created", "12/28/22" },
+                { "updated", null },
+            };
+
+            Assert.ThrowsException<ArgumentException>(() 
+                => new BaseModelTestClass(NullableDictionary.FromDictToNullableDict(data)!));
+        }
+    }
+}
